e61fb52b39e1573157057705737539289bb42746
Pierre Glianos <piereglianos@gmail.com>
2017-11-07 04:13:37 -0800

    Revert "Hello world print"
    This reverts commit 0bf1259c7279b3890fdbd3f4049bc92a83a1d743.

--- core/Main.cc
+++ core/Main.cc
@@ -70,7 +70,6 @@ static void SIGINT_exit(int signum) {
 
 int main(int argc, char** argv)
 {
-	printf("Hello, world!\n");
     try {
         setUsageHelp("USAGE: %s [options] <input-file> <result-output-file>\n\n  where input may be either in plain or gzipped DIMACS.\n");
         // printf("This is MiniSat 2.0 beta\n");
