904a1b9b6d01a224a05d0f188bebff55ecbde0b6
Pierre Glianos <piereglianos@gmail.com>
2017-12-04 22:53:07 +0200

    Added SAT checking methods for the MCA

--- core/AssumMinimiser.h
+++ core/AssumMinimiser.h
@@ -30,27 +30,50 @@ class AssumMinimiser {
     Queue<Lit>   currAssum;
     //Statistics
     int          nSolveCalls, nSAT, nUNSAT;
-    lbool        isSat;         //specifies if the formula is sat w/o assum
+    lbool        isSatWith, isSatWo;         //specifies if the formula is sat w/o assum
 
 public:
-    AssumMinimiser(Solver& s, vec<Lit>& assum) : s(s), initAssum(), currAssum() {
+    AssumMinimiser(Solver& s, vec<Lit>& assum) : s(s), initAssum(), currAssum(), isSatWith(l_Undef),
+                                                 isSatWo(l_Undef) {
         initAssum.copyFrom(assum);
         nSolveCalls = nSAT = nUNSAT = 0;
         currAssum.fromVec(assum);
+        isSatWith = isSatWo = l_Undef;
     }
 
-    void    iterativeDel (vec<Lit> &result);
-    void    PrintStats   () const;
+    /** These two methods checks if the formula is SAT with and without the assumptions
+     * respectively, if it's SAT with the assumptions or UNSAT without the assumptions, then
+     * minimizing the conflicting assumptions is irrelevant or trivial - respectively.
+     */
+    lbool    isSatWithAssum();
+    lbool    isSatWoAssum  ();
+    void    iterativeDel  (vec<Lit> &result);
+    void    PrintStats    () const;
 };
 
+lbool AssumMinimiser::isSatWithAssum() {
+    if (isSatWith == l_Undef) {
+        isSatWith = s.solveLimited(initAssum);
+        if (isSatWith == l_True) isSatWo = l_True;
+    }
+    return isSatWith;
+}
+
+
+lbool AssumMinimiser::isSatWoAssum() {
+    if (isSatWo == l_Undef) {
+        isSatWo = s.solveLimited(vec<Lit>());
+        if (isSatWo == l_False) isSatWith = l_False;
+    }
+    return isSatWo;
+}
 
 void AssumMinimiser::iterativeDel(vec<Lit> &result) {
     Lit p = lit_Undef;
     lbool ret;
     result.clear(false);
     vec<Lit> tmpAssum;
-    if (isSat == l_Undef) isSat = s.solveLimited(vec<Lit>());
-    if (isSat == l_False) return;
+    if (isSatWithAssum() == l_True) return;
 
     /*TODO check that the copy c'tors work as they should initAssum == currAssum */
 
